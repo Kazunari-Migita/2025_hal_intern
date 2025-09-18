84d876f6baf9be8df6c704645e562afbe86ac466
Kazunari-Migita <k-migita@upgarage-g.co.jp>
2025-09-18 12:34:17 +0900

    dart研修の演習解答

---
+++ dart/1_変数とデータ型.dart
@@ -0,0 +1,16 @@
+// 次の変数宣言を完了し、データ型を正確に設定してください。
+void main() {
+  // ①整数を格納する変数を宣言して、値を代入して出力してください。
+  int integerVariable = 42;
+  print(integerVariable);
+
+// ②文字列を格納する変数を宣言して、値を代入して出力してください。
+  String stringVariable = "Hello, Dart!";
+  print(stringVariable);
+
+// ③小数を格納する変数を宣言して、値を代入して出力してください。
+  double doubleVariable = 3.14;
+  print(doubleVariable);
+}
+
+
---
+++ dart/2_条件分岐.dart
@@ -0,0 +1,12 @@
+// 与えられた数が偶数か奇数かを判定するDartのプログラムを作成してください。
+void main() {
+  int number = 7;
+
+  if (number % 2 == 0) {
+    print("$number は偶数です。");
+  } else {
+    print("$number は奇数です。");
+  }
+}
+
+
